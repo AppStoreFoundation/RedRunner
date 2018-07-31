d1a1013b13caf42b14ffc25cc3eeff8998a1dfa0
Nuno Monteiro <nuno.monteiro@aptoide.com>
2018-07-31 12:44:17 +0100

    Integrated latest version of plugin

--- Assets/AppcoinsUnity/Tools/BashCommand.sh
+++ Assets/AppcoinsUnity/Tools/BashCommand.sh
@@ -3,7 +3,7 @@ osascript -e 'activate application "/Applications/Utilities/Terminal.app"'
 cd '/Users/nunomonteiro/Documents/GitHub/RedRunner/and/Red Runner'
 if [ "$('/Users/nunomonteiro/Library/Android/sdk/platform-tools/adb' get-state)" == "device" ]
 then
-'/Users/nunomonteiro/Library/Android/sdk/platform-tools/adb' shell am start -n 'com.asfoundation.redrunner/.UnityPlayerActivity' 2>&1 2>'/Users/nunomonteiro/Documents/GitHub/RedRunner/Assets/AppcoinsUnity/Tools/ProcessError.out'
+'/Users/nunomonteiro/Library/Android/sdk/platform-tools/adb' -d install -r './build/outputs/apk/release/Red Runner-release.apk' 2>&1 2>'/Users/nunomonteiro/Documents/GitHub/RedRunner/Assets/AppcoinsUnity/Tools/ProcessError.out'
 else
 echo error: no usb device found > '/Users/nunomonteiro/Documents/GitHub/RedRunner/Assets/AppcoinsUnity/Tools/ProcessError.out'
 fi
